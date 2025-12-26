8d072a2b0f610f783b8945cf11a3e50aa099f77a
cestmoi <cestmoi@cestmoi.moi>
2025-12-26 23:38:55 +0100

    delete git pull script

--- pull.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Set the path to your Git repository folder
-set REPO_PATH="C:\Users\amarr\OneDrive\Desktop\expertise3000\expertises2"
-
-REM Go to the repository folder
-cd /d "%REPO_PATH%"
-
-git pull
-pause
